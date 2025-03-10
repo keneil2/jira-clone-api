498bb713f9e2fc5898f6deaa907dbeda21d5a003
Keneil-Samms <keneil@innovate10x.com>
2025-03-10 05:27:10 -0500

    added the fillables needed

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -7,4 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     //
+
+    protected $fillable = [
+        "name",
+        "project_template_id",
+        "description",
+        "workspace_id"
+    ];
 }
