5c7c2bcce6283ad5482aa3dbaed0e6f138c8f8db
Keneil Samms <keneilsamms23@gmail.com>
2025-04-24 21:18:28 -0500

    added Enpoints

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\Project\projectController;
+use App\Http\Controllers\Api\Role\RoleController;
+use App\Http\Controllers\Api\Team\TeamController;
 use App\Http\Controllers\Api\user\UserController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Project\ProjectUsersControllers;
 use App\Http\Controllers\Workspace\ProjectTemplateController;
 use App\Http\Controllers\Workspace\WorkspaceController;
 use App\Http\Controllers\Workspace\WorkspaceTemplateController;
@@ -27,6 +30,34 @@ Route::get('/check-workspace',[UserController::class,"userCreatedWorkspace"]);
 Route::get("project-templates/all",[ ProjectTemplateController::class,"all"]); 
 Route::post('create-project',[projectController::class,"create"]);
 Route::post('create-workspace',[WorkspaceController::class,"create"]);
+
+
+Route::prefix("project")->group(function(){
 Route::get("/user-projects",[projectController::class,"getUserProject"]);
+Route::get("/{projectID}",[projectController::class,"getProjectById"]);
+Route::get("/{projectID}/members",[projectController::class,"getUsers"]);
+Route::get("/{projectID}/create-task",[projectController::class,"getUsers"]);
+
+});
+
+Route::prefix("workspace")->group(function(){
+Route::get("users",[WorkspaceController::class]);
+});
+
+Route::prefix("role")->group(function(){
+    Route::get("all",[RoleController::class,"getAll"]);
+   
+});
+
+Route::prefix("user")->group(function(){
+    Route::get("mention_slug",[UserController::class ,"getUsersByMention"]);
+});
+
+Route::prefix("team")->group(function(){
+    Route::post("create",[TeamController::class,"createTeam"]);
+    Route::get("/{workspace_id}",[TeamController::class,"getTeams"]);
+});
+
+
 });
 
