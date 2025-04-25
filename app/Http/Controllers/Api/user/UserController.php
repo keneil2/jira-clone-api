078fd62b79df29e765e94ed8b2d562dc6f0514ac
Keneil Samms <keneilsamms23@gmail.com>
2025-04-24 21:05:50 -0500

    Updated UserController
    added a function to fetch user by mention

--- app/Http/Controllers/Api/user/UserController.php
+++ app/Http/Controllers/Api/user/UserController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Api\user;
 
 use App\Http\Controllers\Controller;
+use App\Http\Response\ApiResponse;
+use App\Models\User;
 use App\Models\Workspace;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -18,5 +22,24 @@ class UserController extends Controller
            return false;
         }
         return true;
+    }
+    public function getUsersByMention(Request $request){
+        try {
+         // getting the query
+        $query=$request->query("handle");
+        if($query){
+$users =User::where("mention_slug","LIKE",$query."%")->get()->toArray();
+return ApiResponse::ok($users,"Users retrived Successfully");
+        }
+        } catch (\Exception $th) {
+            Log::debug($th);
+            ApiResponse::serverError();
+        }
+       
+
+        // Validator::make($mention_slug,[
+             
+        // ]);
+        // 
     }
 }
