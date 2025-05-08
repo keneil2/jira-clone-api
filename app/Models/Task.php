55557bc545dd41ab50756b92b1eddea398305f86
Keneil Samms <keneilsamms23@gmail.com>
2025-05-08 08:40:45 -0500

    Updated Task Model with required Fillables

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable=[
+"name",
+ "priority",
+ "assignee_id" ,
+"project_id" ,
+"reference_no",// to be implemented
+ "task_type_id",
+ "description",
+ 'due_date'
+    ];
 }
